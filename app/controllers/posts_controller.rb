d6cd38122a146629849887be8a469a063d983564
Syed Anab Imam <syedanabimam@gmail.com>
2016-02-10 20:24:44 +0000

    Added new test cases and updating feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     def index  
+       @posts = Post.all 
     end  
     
     def new
@@ -19,6 +20,16 @@ class PostsController < ApplicationController
     
     def show
        @post = Post.find(params[:id]) 
+    end
+    
+    def edit
+       @post = Post.find(params[:id])  
+    end
+    
+    def update
+       @post = Post.find(params[:id])
+       @post.update(post_params)
+       redirect_to(post_path(@post)) 
     end
     
     private
