1e90668377e8a5e913da5074241e606030a56fec
Syed Anab Imam <syedanabimam@gmail.com>
2016-02-10 23:31:56 +0000

    Added styling to show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :post_type_select, presence: true
   
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
